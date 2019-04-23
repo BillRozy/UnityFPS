ae0716b22d895e52481759c85f420990a8a8eb5d
Oleg Budylin <Oleg.Budylin@harman.com>
2019-04-23 14:26:47 +0300

    fps input

---
+++ Assets/Scripts/Controllers/FPSInput.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Scripts/FPS Input")]
+public class FPSInput : MonoBehaviour
+{
+    public float speed = 5f;
+    public float gravity = -9.8f;
+    public float jumpEnergy = 15f;
+    public float jumpDuration = 0.3f;
+
+    private bool _isJumping = false;
+    private float _lastJumpTime = 0;
+
+    private CharacterController _charController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lastJumpTime = Time.time;
+        _charController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time - _lastJumpTime > jumpDuration) {
+            _isJumping = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (!_isJumping) {
+                _isJumping = true;
+                _lastJumpTime = Time.time;
+            }
+        }
+        float dx = Input.GetAxis("Horizontal") * speed;
+        float dz = Input.GetAxis("Vertical") * speed;
+        Vector3 movement = new Vector3(dx, 0, dz);
+        movement = Vector3.ClampMagnitude(movement, speed);
+        movement.y = gravity;
+        if (_isJumping) movement.y += jumpEnergy;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+        _charController.Move(movement);
+    }
+}
---
+++ Assets/Scripts/Controllers/MouseLook.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public enum RotationAxes {
+        MouseXAndY = 0,
+        MouseX = 1,
+        MouseY = 2
+    }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float horSens = 200f;
+    public float verSens = 200f;
+    public float[] vertClamp = {-45f, 45f};
+
+    private float _rotationX = 0;
+    private float _rotationY = 0;
+
+
+    void Start() {
+        Rigidbody body = GetComponent<Rigidbody>();
+        if (body != null) body.freezeRotation = true;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseX) {
+            _rotationY += Input.GetAxis("Mouse X") * horSens * Time.deltaTime;
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, _rotationY , 0);
+        } else if (axes == RotationAxes.MouseY) {
+            _rotationX -= Input.GetAxis("Mouse Y") * verSens * Time.deltaTime;
+            _rotationX = Mathf.Clamp(_rotationX, vertClamp[0], vertClamp[1]);
+            transform.localEulerAngles = new Vector3(_rotationX, transform.localEulerAngles.y, 0);
+        } else {
+            _rotationY += Input.GetAxis("Mouse X") * horSens * Time.deltaTime;
+            _rotationX -= Input.GetAxis("Mouse Y") * verSens * Time.deltaTime;
+            _rotationX = Mathf.Clamp(_rotationX, vertClamp[0], vertClamp[1]);
+            transform.localEulerAngles = new Vector3(_rotationX, _rotationY, 0);
+        }
+    }
+}
