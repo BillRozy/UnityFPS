66f4a517c09604e33b803362e5f2d66118066fa7
Oleg Budylin <Oleg.Budylin@harman.com>
2019-04-23 17:40:40 +0300

    1 chapter and raycasting started

---
+++ Assets/Scripts/Controllers/Shooting.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private Camera _cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cam = GetComponentInChildren<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void OnGUI() {
+        int size = 12;
+        float posX = _cam.pixelWidth / 2 - size / 4;
+        float posY = _cam.pixelHeight / 2 - size / 4;
+        GUI.Label(new Rect(posX, posY, size, size), "*");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Vector3 point = new Vector3(_cam.pixelWidth / 2, _cam.pixelHeight / 2, 0);
+            Ray ray = _cam.ScreenPointToRay(point);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit)) {
+                GameObject hitObj = hit.transform.gameObject;
+                ReactiveTarget target = hitObj.GetComponent<ReactiveTarget>();
+                if (target != null) {
+                    target.ReactToHit();
+                } else {
+                    StartCoroutine(SphereIndicator(hit.point));
+                }
+            }
+        }
+    }
+
+    private IEnumerator SphereIndicator(Vector3 pos) {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = pos;
+        yield return new WaitForSeconds(1);
+        Destroy(sphere);
+    }
+}
---
+++ Assets/Scripts/Controllers/WanderingAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingAI : MonoBehaviour
+{
+    public float speed = 1f;
+    public float obstacleRange = 3f;
+
+    private bool _alive = true;
+    public bool alive {
+        private get {
+            return _alive;
+        }
+        set {
+            _alive = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (alive) {
+            transform.Translate(0, 0, speed * Time.deltaTime);
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.SphereCast(ray, 0.75f, out hit)) {
+                if (hit.distance < obstacleRange) {
+                    float angle = Random.Range(-110, 110);
+                    transform.Rotate(0, angle, 0);
+                }
+            }
+        }
+    }
+}
